e4395302566f5738f78ca6d42b0a3d419568b294
NguyenTanPil <traila1008@gmail.com>
2022-03-25 16:25:50 +0700

    fetch category of recipe and count by category

--- client/src/components/RecipeCategories/index.jsx
+++ client/src/components/RecipeCategories/index.jsx
@@ -5,8 +5,68 @@ import {
   ShortLine,
 } from '../TrendingRecipes/TrendingRecipesStyles';
 import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { collection, getDocs, query } from 'firebase/firestore';
+import db from '../../firebase';
 
 const RecipeCategories = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    let isSubscribed = true;
+
+    const fetchCategories = async () => {
+      let response = [];
+
+      try {
+        const queryRecipes = query(collection(db, 'recipes'));
+
+        const querySnapshot = await getDocs(queryRecipes);
+
+        querySnapshot.forEach((doc) => {
+          const currentCategory = response.find((category) => {
+            return category.name === doc.data().category;
+          });
+
+          if (currentCategory) {
+            response = response.map((category) => {
+              if (category.name === currentCategory.name) {
+                return { ...category, count: category.count + 1 };
+              }
+              return category;
+            });
+          } else {
+            response.push({
+              name: doc.data().category,
+              count: 1,
+            });
+          }
+        });
+      } catch (error) {
+        console.log('Error: ' + error.message);
+      }
+
+      if (isSubscribed) {
+        response.sort((a, b) => {
+          if (a.name > b.name) {
+            return 1;
+          }
+          if (b.name > a.name) {
+            return -1;
+          }
+          return 0;
+        });
+        setCategories(response);
+      }
+    };
+
+    fetchCategories();
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, []);
+
   return (
     <Container>
       <Header>
@@ -14,38 +74,15 @@ const RecipeCategories = () => {
         <ShortLine />
       </Header>
       <Categories>
-        <CategoryItem>
-          <NavLink to="/breakfast">Breakfast</NavLink>
-          <span>(3)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/lunch">Lunch</NavLink>
-          <span>(2)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/dinner">dinner</NavLink>
-          <span>(2)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/drinks">Drinks</NavLink>
-          <span>(3)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/starter">Starter</NavLink>
-          <span>(1)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/side-dish">side dish</NavLink>
-          <span>(1)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/dessert">Dessert</NavLink>
-          <span>(3)</span>
-        </CategoryItem>
-        <CategoryItem>
-          <NavLink to="/snack">Snack</NavLink>
-          <span>(2)</span>
-        </CategoryItem>
+        {categories.length > 0 &&
+          categories.map((category, index) => (
+            <CategoryItem key={index}>
+              <NavLink to={`/${category.name.replace(' ', '-')}`}>
+                {category.name}
+              </NavLink>
+              <span>({category.count})</span>
+            </CategoryItem>
+          ))}
       </Categories>
     </Container>
   );
